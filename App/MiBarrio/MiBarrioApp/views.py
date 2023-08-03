2ede2baad7ddcfaa442f6b4adc707633a4eca402
Maria Palacios-Ricaldi <124238922+maria-palacios-ricaldi@users.noreply.github.com>
2023-08-03 21:22:00 +0200

    Update views.py

--- App/MiBarrio/MiBarrioApp/views.py
+++ App/MiBarrio/MiBarrioApp/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index_page(request) :
+#def index_page(request) :
     #return HttpResponse("First App")
-    return render(request, "index.html", {})
+    #return render(request, "index.html", {})
+
+def home_view(request):
+    return render(request, 'home.html', {})
+
+def login_view(request):
+    return render(request, 'login.html', {})
