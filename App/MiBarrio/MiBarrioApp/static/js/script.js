bf0b2f881c18e58553764389be0758319da8abe1
Maria Palacios-Ricaldi <124238922+maria-palacios-ricaldi@users.noreply.github.com>
2023-08-13 22:44:42 +0200

    Update script.js

--- App/MiBarrio/MiBarrioApp/static/js/script.js
+++ App/MiBarrio/MiBarrioApp/static/js/script.js
@@ -1,9 +1,17 @@
 //script.js//
+$(document).ready(function() {
+  $("#toggleSidebar").click(function(e) {
+    e.preventDefault();
+    $("#sidebar").toggleClass("collapsed");
+
+    // Use Bootstrap 5 utility classes for toggling visibility
+    if ($("#sidebar").hasClass("collapsed")) {
+      $(".nav-text").addClass("d-none");
+    } else {
+      $(".nav-text").removeClass("d-none");
+    }
+  });
+});
 
 
 //toggle navbar - collapse
-$(document).ready(function() {
-  $("#toggleSidebar").click(function() {
-      $("#sidebar").toggleClass("collapsed");
-  });
-});
