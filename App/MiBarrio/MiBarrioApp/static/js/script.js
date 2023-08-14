671578974a28d389f87d71b85ed356fef2ef1c98
Maria Palacios-Ricaldi <124238922+maria-palacios-ricaldi@users.noreply.github.com>
2023-08-14 22:00:56 +0200

    Update script.js

--- App/MiBarrio/MiBarrioApp/static/js/script.js
+++ App/MiBarrio/MiBarrioApp/static/js/script.js
@@ -20,3 +20,31 @@ $(document).ready(function() {
   console.log("Script loaded!");  // This should appear in your browser's console
   // ... rest of the code ...
 });
+
+
+document.getElementById('editSaveBtn').addEventListener('click', function() {
+  if (this.innerText === 'Edit') {
+      this.innerText = 'Save';
+      document.getElementById('cancelBtn').classList.remove('disabled');
+      document.querySelectorAll('#accountDetailsForm input:not(#username)').forEach(input => {
+          input.disabled = false;
+      });
+      document.getElementById('repasswordDiv').style.display = 'block';
+  } else {
+      this.innerText = 'Edit';
+      document.getElementById('cancelBtn').classList.add('disabled');
+      document.querySelectorAll('#accountDetailsForm input:not(#username)').forEach(input => {
+          input.disabled = true;
+      });
+      document.getElementById('repasswordDiv').style.display = 'none';
+  }
+});
+
+document.getElementById('cancelBtn').addEventListener('click', function() {
+  document.getElementById('editSaveBtn').innerText = 'Edit';
+  this.classList.add('disabled');
+  document.querySelectorAll('#accountDetailsForm input:not(#username)').forEach(input => {
+      input.disabled = true;
+  });
+  document.getElementById('repasswordDiv').style.display = 'none';
+});
