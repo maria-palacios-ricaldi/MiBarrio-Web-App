26e9a09ace9ab3778b1fb695ce9bd7f56980f85d
Maria Palacios-Ricaldi <124238922+maria-palacios-ricaldi@users.noreply.github.com>
2023-09-05 11:36:48 +0200

    Update forms.py

--- App/MiBarrio/MiBarrioApp/forms.py
+++ App/MiBarrio/MiBarrioApp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import SearchProfile
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -19,3 +20,11 @@ class CustomUserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class SearchProfileForm(forms.ModelForm):
+    class Meta:
+        model = SearchProfile
+        fields = ['sp_name', 'age', 'social_cultural', 'health_wellness',
+                  'leisure_recreation', 'community_services',
+                  'transportation_active', 'transportation_public',
+                  'backup_power_supply', 'backup_water_supply']
