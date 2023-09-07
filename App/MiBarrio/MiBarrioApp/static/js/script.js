d7e5f73daf7b6893d9c04ba240e3443431130649
Maria Palacios-Ricaldi <124238922+maria-palacios-ricaldi@users.noreply.github.com>
2023-09-07 13:21:44 +0200

    Update script.js

--- App/MiBarrio/MiBarrioApp/static/js/script.js
+++ App/MiBarrio/MiBarrioApp/static/js/script.js
@@ -94,4 +94,97 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
+  //handles the population of the form for search profile for the user to edit
+  document.querySelectorAll('.edit-btn').forEach((button) => {
+    button.addEventListener('click', function() {
+      const spId = this.getAttribute('data-id');
+      const spName = this.getAttribute('data-sp_name');
+      const age = this.getAttribute('data-age');
+      const social_cultural_tags = this.getAttribute('data-social_cultural_tags');
+      const health_wellness_tags = this.getAttribute('data-health_wellness_tags');
+      const leisure_recreation_tags = this.getAttribute('data-leisure_recreation_tags');
+      const community_services_tags = this.getAttribute('data-community_services_tags');
+      const social_cultural_levels = this.getAttribute('data-social_cultural_levels');
+      const health_wellness_levels = this.getAttribute('data-health_wellness_levels');
+      const leisure_recreation_levels = this.getAttribute('data-leisure_recreation_levels');
+      const community_services_levels = this.getAttribute('data-community_services_levels');
+      const transportation_active = this.getAttribute('data-transportation_active');
+      const transportation_public = this.getAttribute('data-transportation_public');
+      const backup_power_supply = this.getAttribute('data-backup_power_supply');
+      const backup_water_supply = this.getAttribute('data-backup_water_supply');
+
+      // Populate the form fields
+      document.getElementById('sp_name').value = spName;
+      document.getElementById('age').value = age;
+
+      // Populate checkboxes for Social & Cultural tags
+      let socialTags = JSON.parse(social_cultural_tags);  // Parse JSON string to array
+      socialTags.forEach(tag => {
+        let checkbox = document.querySelector(`input[name='social_cultural[]'][value='${tag}']`);
+        if (checkbox) checkbox.checked = true;
+      });
+
+      // Populate checkboxes for Health & Wellness tags
+      let healthTags = JSON.parse(health_wellness_tags);
+      healthTags.forEach(tag =>{
+        let checkbox = document.querySelector(`input[name='health_wellness[]'][value='${tag}']`);
+        if (checkbox) checkbox.checked = true;
+      });
+
+      // Populate checkboxes for Leisure & Recreation tags
+      let leisureTags = JSON.parse(leisure_recreation_tags);
+      leisureTags.forEach(tag =>{
+        let checkbox = document.querySelector(`input[name='leisure_recreation[]'][value='${tag}']`);
+        if (checkbox) checkbox.checked = true;
+      });
+
+      // Populate checkboxes for Health & Wellness tags
+      let communityTags = JSON.parse(community_services_tags);
+      communityTags.forEach(tag =>{
+        let checkbox = document.querySelector(`input[name='community_services[]'][value='${tag}']`);
+        if (checkbox) checkbox.checked = true;
+      });
+
+      // Populate radio button for Social & Cultural Levels
+      let socialLevel = document.querySelector(`input[name='socialLikert'][value='${social_cultural_levels}']`);
+      if (socialLevel) socialLevel.checked = true;
+
+      // Populate radio button for Health & Wellness Levels
+      let healthLevel = document.querySelector(`input[name='healthLikert'][value='${health_wellness_levels}']`);
+      if (healthLevel) healthLevel.checked = true;
+
+      // Populate radio button for Health & Wellness Levels
+      let leisureLevel = document.querySelector(`input[name='leisureLikert'][value='${leisure_recreation_levels}']`);
+      if (leisureLevel) leisureLevel.checked = true;
+
+      // Populate radio button for Health & Wellness Levels
+      let communityLevel = document.querySelector(`input[name='communityLikert'][value='${community_services_levels}']`);
+      if (communityLevel) communityLevel.checked = true;
+
+       // Populate checkboxes for Active and Public Transportation
+      if (transportation_active === 'True') {
+      document.getElementById('activeTransportation').checked = true;
+      }
+
+      if (transportation_public === 'True') {
+        document.getElementById('publicTransportation').checked = true;
+      }
+
+      // Populate checkboxes for Back-up Power Supply and Water Supply
+      if (backup_power_supply === 'True') {
+      document.getElementById('backup_power_supply').checked = true;
+      }
+      if (backup_water_supply === 'True') {
+      document.getElementById('backup_water_supply').checked = true;
+      }
+
+
+
+
+      // Update form action to handle editing
+      const form = document.getElementById('searchProfileForm');
+      form.setAttribute('action', `/edit_search_profile/${spId}`);
+    });
+  });
+
 });
