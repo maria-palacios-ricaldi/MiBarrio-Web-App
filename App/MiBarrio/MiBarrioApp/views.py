e644e0f365ef9b6d5049bdf05b228ec7d0c8276a
Maria Palacios-Ricaldi <124238922+maria-palacios-ricaldi@users.noreply.github.com>
2023-09-08 21:24:42 +0200

    Update views.py

--- App/MiBarrio/MiBarrioApp/views.py
+++ App/MiBarrio/MiBarrioApp/views.py
@@ -6,6 +6,8 @@ from django.contrib import messages
 from .models import SearchProfiles
 from django.http import JsonResponse
 import json
+from django.core.serializers.json import DjangoJSONEncoder
+
 
 # Create your views here.
 
@@ -56,6 +58,8 @@ def profile_view(request):
             return redirect('MiBarrioApp:profile')
 
         elif form_type == 'search_profile':
+            editingSpId = request.POST.get('editingSpId')
+
             print("Processing search_profile form")
 
             sp_name = request.POST.get('sp_name')
@@ -86,9 +90,38 @@ def profile_view(request):
             backup_power_supply = request.POST.get('backup_power_supply') == 'on'
             backup_water_supply = request.POST.get('backup_water_supply') == 'on'
 
-            try:
-                print("Trying to save search profile...")  # Add this
-                search_profile = SearchProfiles.objects.create(
+            # Function to update or create a search profile
+            def save_search_profile(sp=None):
+                sp.sp_name = sp_name
+                sp.age = age
+                sp.social_cultural_tags = json.dumps(social_cultural)
+                sp.health_wellness_tags = json.dumps(health_wellness)
+                sp.leisure_recreation_tags = json.dumps(leisure_recreation)
+                sp.community_services_tags = json.dumps(community_services)
+                sp.social_cultural_levels = social_cultural_levels
+                sp.health_wellness_levels = health_wellness_levels
+                sp.leisure_recreation_levels = leisure_recreation_levels
+                sp.community_services_levels = community_services_levels
+                sp.transportation_active = transportation_active
+                sp.transportation_public = transportation_public
+                sp.backup_power_supply = backup_power_supply
+                sp.backup_water_supply = backup_water_supply
+                sp.save()
+
+
+            if editingSpId:
+                try:
+                    search_profile = SearchProfiles.objects.get(search_profileID=editingSpId)
+                    save_search_profile(search_profile)
+                    messages.success(request, 'Your search profile has been updated!')
+                except SearchProfiles.DoesNotExist:
+                    messages.error(request, 'Search profile not found.')
+
+            else:
+                #Creating a new search profile
+                try:
+                    print("Trying to save search profile...")  # Add this
+                    search_profile = SearchProfiles.objects.create(
                     user=user,
                     sp_name=sp_name,
                     age=age,
@@ -106,11 +139,13 @@ def profile_view(request):
                     backup_power_supply=backup_power_supply,
                     backup_water_supply=backup_water_supply
                 )
-                search_profile.save()
-                messages.success(request, 'Your search profile has been saved!')
-            except Exception as e:
-                print(f"An error occurred while saving the search profile: {e}")
-                messages.error(request, 'An error occurred while saving your search profile.')
+                    save_search_profile(search_profile)
+                    messages.success(request, 'Your search profile has been saved!')
+
+                except Exception as e:
+                    # Log the exception
+                    print(f"An error occurred while saving the search profile: {e}")
+                    messages.error(request, 'An error occurred while saving your search profile.')
 
             return redirect('MiBarrioApp:profile')
 
@@ -121,7 +156,57 @@ def profile_view(request):
 
 
 def new_search_view(request):
-    return render(request, 'newSearch.html', {})
+    if request.user.is_authenticated:
+        search_profiles = SearchProfiles.objects.filter(user=request.user)
+
+        # Check if request is POST and has selected_profile
+        if request.method == 'POST':
+            selected_profile_id = request.POST.get('selected_profile', None)
+            request.session['selected_profile_id'] = selected_profile_id
+        else:
+            selected_profile_id = request.session.get('selected_profile_id', None)
+
+        # Fetch the selected profile based on the profile_id
+        if selected_profile_id:
+            selected_profile = SearchProfiles.objects.filter(user=request.user, search_profileID=selected_profile_id).first()
+        else:
+            selected_profile = search_profiles.first()
+
+        print("Debug: selected_profile_id:", selected_profile_id)
+
+        if selected_profile:
+            selected_profile_data = {
+                'search_profileID': selected_profile.search_profileID,
+                'age': selected_profile.age,
+                'social_cultural_tags': json.loads(selected_profile.social_cultural_tags),
+                'health_wellness_tags': json.loads(selected_profile.health_wellness_tags),
+                'leisure_recreation_tags': json.loads(selected_profile.leisure_recreation_tags),
+                'community_services_tags': json.loads(selected_profile.community_services_tags),
+                'social_cultural_levels': selected_profile.social_cultural_levels,
+                'health_wellness_levels': selected_profile.health_wellness_levels,
+                'leisure_recreation_levels': selected_profile.leisure_recreation_levels,
+                'community_services_levels': selected_profile.community_services_levels,
+                'transportation_active': selected_profile.transportation_active,
+                'transportation_public': selected_profile.transportation_public,
+            }
+            context = {
+                'search_profiles': search_profiles,
+                'selected_profile_data': json.dumps(selected_profile_data, cls=DjangoJSONEncoder, ensure_ascii=True).replace('"', '&quot;'),
+                'selected_profile_id': selected_profile_id
+            }
+
+
+
+        else:
+            context = {
+                'search_profiles': search_profiles
+            }
+
+        print("Debug: selected_profile_data: ", json.dumps(selected_profile_data, cls=DjangoJSONEncoder))
+
+        return render(request, 'newSearch.html', context)
+    else:
+        return redirect('login')
 
 def new_search_2_view(request):
     return render(request, 'newSearch2.html', {})
