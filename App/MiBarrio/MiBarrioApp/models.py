cb37603278b1ff76fc38f63b76008caa36fa0b1a
Maria Palacios-Ricaldi <124238922+maria-palacios-ricaldi@users.noreply.github.com>
2023-09-14 00:14:25 +0200

    Update models.py

--- App/MiBarrio/MiBarrioApp/models.py
+++ App/MiBarrio/MiBarrioApp/models.py
@@ -102,7 +102,6 @@ class Suburbs(models.Model):
     coordinates = models.TextField()
 
 
-
 class Properties(models.Model):
     propertyID = models.AutoField(primary_key=True)
     suburb = models.ForeignKey(Suburbs, on_delete=models.CASCADE)
