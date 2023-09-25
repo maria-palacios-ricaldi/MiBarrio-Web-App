bef4fc4db0955787228b96731c2403e622282ff7
Maria Palacios-Ricaldi <124238922+maria-palacios-ricaldi@users.noreply.github.com>
2023-09-25 16:59:11 +0200

    Update settings.py

--- App/MiBarrio/MiBarrio/settings.py
+++ App/MiBarrio/MiBarrio/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'MiBarrioApp.context_processors.show_user_info',
             ],
         },
     },
