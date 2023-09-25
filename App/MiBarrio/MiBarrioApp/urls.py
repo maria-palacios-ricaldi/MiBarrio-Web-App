1667fc3adff61bd2dd5ee859b5638cc290314c16
Maria Palacios-Ricaldi <124238922+maria-palacios-ricaldi@users.noreply.github.com>
2023-09-25 21:11:16 +0200

    Update urls.py

--- App/MiBarrio/MiBarrioApp/urls.py
+++ App/MiBarrio/MiBarrioApp/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
+from MiBarrioApp.forms import MyPasswordResetForm
 from .views import *
 
 app_name = "MiBarrioApp"
@@ -18,11 +19,20 @@ urlpatterns = [
     # password control
     path('change-password/', auth_views.PasswordChangeView.as_view(template_name='change_password.html'), name="change-password"),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_reset_done.html'), name='password_change_done'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='forgot_password.html', subject_template_name='password_reset_subject.txt', html_email_template_name='password_reset_email.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
-
+    path(
+        'password_reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='forgot_password.html',
+            subject_template_name='password_reset_subject.txt',
+            email_template_name='password_reset_email.html',
+            form_class=MyPasswordResetForm,  # Specify custom form class here
+            success_url=reverse_lazy('MiBarrioApp:password_reset_done')  # Updated success_url
+        ),
+        name='password_reset'
+    ),
     #other web app pages
     path('aboutUs', views.about_us_view, name ='aboutUs'),
     path('profile', views.profile_view, name = "profile"),
